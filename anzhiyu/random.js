da76326889c6a64a9d988310105022c872602c4a
xuimianban <xuimianban@proton.me>
2024-07-20 22:25:55 +0800

    Site updated: 2024-07-20 22:25:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/14/hello-world/"];function toRandomPost(){
+var posts=["2024/07/20/搭建01/","2024/07/14/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
