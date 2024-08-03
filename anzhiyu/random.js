01275560e8cc965a25182c2e48713dd5a496d130
xuimianban <xuimianban@proton.me>
2024-08-03 18:55:30 +0800

    Site updated: 2024-08-03 18:55:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/20/hello-world/","2024/07/20/搭建01/"];function toRandomPost(){
+var posts=["2024/07/20/搭建01/","2024/07/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
