da2c2a9dba7437e7c178e6d2de6961f66d10bc24
xuimianban <xuimianban@proton.me>
2024-08-14 19:48:46 +0800

    Site updated: 2024-08-14 19:48:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/20/搭建01/","2024/08/08/代码/","2024/07/20/hello-world/"];function toRandomPost(){
+var posts=["2024/07/20/搭建01/","2024/08/14/纯小注册使用白教程/","2024/08/08/代码/","2024/07/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
