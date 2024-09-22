a45e8d8dde7a6acd9c41237799a1152c0171a609
xuimianban <xuimianban@proton.me>
2024-09-22 16:46:52 +0800

    Site updated: 2024-09-22 16:46:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/14/纯小注册使用白教程/","2024/07/20/搭建01/","2024/08/18/代码相关serv00/","2024/08/08/代码/","2024/07/20/hello-world/"];function toRandomPost(){
+var posts=["2024/08/14/纯小注册使用白教程/","2024/07/20/搭建01/","2024/09/22/青龙面板同步终极/","2024/08/18/代码相关serv00/","2024/08/08/代码/","2024/07/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
